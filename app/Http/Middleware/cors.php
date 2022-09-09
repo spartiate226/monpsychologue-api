9b1016a3996e2f6fb1164d979f5cf81a43c308ec
spartiate226 <eouompeba@gmail.com>
2022-09-09 10:24:41 +0000

    'hgf'

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -16,7 +16,9 @@ class cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
     }
 }
